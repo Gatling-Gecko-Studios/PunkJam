a57b487c1de7e4bb371f0af67c9f3f0214d8f2c8
Pepijn Geerlinks <48803453+Pepsicle@users.noreply.github.com>
2024-01-30 15:10:47 +0100

    Start of day cycle

---
+++ Assets/Code/DayScripts/DayCycle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+    [Header("Day cycle values")]
+    public Gradient lightGradient;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
